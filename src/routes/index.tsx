537bb9f6933e713a43cb769304114d5f0852393a
Guilherme Konopatzki <briares65@gmail.com>
2022-05-16 12:59:30 -0300

    Menu lateral evoluido

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,21 @@ import { BrowserRouter } from "react-router-dom";
 import { Button } from "@mui/material";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts";
+import { useEffect } from "react";
 
 
 export const AppRoutes = () => {
-    const {toggleDrawerOpen} = useDrawerContext();
+    const {toggleDrawerOpen, setDrawerOptions} = useDrawerContext();
+
+    useEffect(() => {
+        setDrawerOptions([
+            {
+                label: 'Página inicial',
+                icon:'home',
+                path: '/pagina-inicial',
+            }
+        ]);
+    }, [])
 
     return (
         <Routes>
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,8 +1,18 @@
 import {createContext, useState, useCallback, useContext} from 'react';
 
+
+interface IDrawerOption {
+    icon: string;
+    path: string;
+    label: string;
+}
+
+
 interface IDrawerContextData {
     isDrawerOpen: boolean;
+    drawerOptions: IDrawerOption[];
     toggleDrawerOpen: () => void;
+    setDrawerOptions: (newDrawerOptions: IDrawerOption[]) => void;
 
 }
 
@@ -19,12 +29,15 @@ interface Prips {
 export const DrawerProvider: React.FC<Prips> = ({children}) => {
 
     const [ isDrawerOpen, setIsDrawerOpen] = useState(false);
-    
+    const [ drawerOptions, setDrawerOptions] = useState<IDrawerOption[]>([]);
+
     const toggleDrawerOpen = useCallback(() => {setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen)}, []);
+
+    const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOption[]) => {setDrawerOptions(newDrawerOptions)}, []); {/* essa função recebe por parÂmetro novas opções de menu */}
 
     
     return(
-        <DrawerContext.Provider value = {{ isDrawerOpen, toggleDrawerOpen}}>
+        <DrawerContext.Provider value = {{ setDrawerOptions: handleSetDrawerOptions, isDrawerOpen, drawerOptions, toggleDrawerOpen}}>
             {children}
         </DrawerContext.Provider>
     );
