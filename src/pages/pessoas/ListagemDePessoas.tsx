46b550061296b229c72996ef83aeda7ca7521bcd
Guilherme Konopatzki <briares65@gmail.com>
2022-06-11 19:13:20 -0300

    Tabela de pessoas, loading e listagem vazia

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina"
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { IListagemPessoa, PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 import { useDebounce } from "../../shared/hooks";
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material";
+import { Environment } from "../../shared/enviroment";
 
 
 
@@ -11,9 +13,11 @@ import { useDebounce } from "../../shared/hooks";
 export const ListagemDePessoas: React.FC = () => {
 
     const [searchParams, setSearchParamns] = useSearchParams();
+    const { debounce } = useDebounce();
 
-
-    const { debounce } = useDebounce(3000, false);
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
 
     const busca = useMemo(() => {
@@ -22,15 +26,22 @@ export const ListagemDePessoas: React.FC = () => {
 
 
     useEffect(() => {
+        setIsLoading(true);
 
         debounce(() => {
             PessoasService.getAll(1, busca)
             .then((result) => {
+            setIsLoading(false); ////Para de mostrar o feedback visual para o usuário}
+            
             if (result instanceof Error){
                 alert(result.message);
                 return;
             }
             console.log(result);
+
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
         })
 
         
@@ -48,7 +59,44 @@ export const ListagemDePessoas: React.FC = () => {
                 aoMudarTextoDeBusca={texto => setSearchParamns({ busca: texto}, {replace:true})} 
             />}        
         >
+            <TableContainer component={Paper} variant="outlined" sx={{ margin: 1 , width:"auto"}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome Completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
 
+                    <TableBody>
+                    {rows.map(row => (
+                    <TableRow key={row.id}>
+                        <TableCell>Ações</TableCell>
+                        <TableCell>{row.nomeCompleto}</TableCell>
+                        <TableCell>{row.email}</TableCell>
+                    </TableRow>
+                    ))}
+                    </TableBody>
+
+                        {totalCount === 0 && !isLoading &&(
+                            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+                        )}
+
+                    <TableFooter>
+                    {isLoading && (
+                        <TableRow>
+                            <TableCell colSpan={3}> 
+                                
+                                    <LinearProgress variant="indeterminate"/>
+                                
+                            </TableCell>
+                        </TableRow>
+                    )}
+                    </TableFooter>
+
+                </Table>
+            </TableContainer>
         </LayoutBaseDePagina>    
     );
 };
